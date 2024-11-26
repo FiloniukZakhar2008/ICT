cba33d18e890e79f75340b12b0ff9088230ab9c3
FiloniukZakhar2008 <lazzyyyx52@gmail.com>
2024-11-26 11:51:10 +0200

    Ехобот.py

--- Ехоботік.py
+++ Ехоботік.py
@@ -7,6 +7,6 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.text)
-
+ 
 if __name__ == '__main__':
      bot.infinity_polling()
